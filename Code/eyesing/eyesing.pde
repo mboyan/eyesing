96388de6fddfc596474fe3ba289574941f18337d
Boyan Mihaylov <mih.boyan@gmail.com>
2025-02-05 23:02:24 +0100

    noise_shader not working

--- Code/eyesing/eyesing.pde
+++ Code/eyesing/eyesing.pde
@@ -1,21 +1,38 @@
-PShader shader;
+PShader shader, noiseShader;
+PGraphics noiseGraphics;
 
 void setup(){
   
+  size(500, 500, P2D);
+  //fullScreen(P2D);
+  
+  //noiseGraphics = createGraphics(width, height, P2D);
+  
+  noiseShader = loadShader("noise_shader.glsl");
+  noiseShader.set("iResolution", float(width), float(height), 0.0);
+  
   shader = loadShader("eyesing_shader.glsl");
   shader.set("iResolution", float(width), float(height), 0.0);
-  
-  //size(500, 500, P2D);
-  fullScreen(P2D);
+  //shader.set("iChannel0", noiseImg);
   
 }
 
 
 void draw(){
   
+  // Update noise shader
+  noiseShader.set("iTime", frameCount);
+  
+  //noiseGraphics.beginDraw();
+  //noiseGraphics.shader(noiseShader);
+  //noiseGraphics.rect(0, 0, width, height);
+  //noiseGraphics.endDraw();
+  
+  // Update processing shader
   shader.set("iTime", float(frameCount));
   
   shader(shader);
+  //image(noiseGraphics, 0, 0);
   fill(0);
   rect(0, 0, width, height);
   
--- Code/eyesing/noise_shader.glsl
+++ Code/eyesing/noise_shader.glsl
@@ -18,12 +18,35 @@ uniform int       iFrame;                // shader playback frame
 uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
 uniform vec4      iDate;                 // (year, month, day, time in seconds)
 
-varying vec4 vertColor;
-
-uniform sampler2D iChannel0;
 
 #define PI 3.14159265358979323846
 
+float rand(vec2 st) {
+    st = fract(st * 0.3183099);  // Scale to make the randomness denser
+    st += dot(st, st + 33.333);   // More mixing
+    return fract(st.x * st.y * 12345.6789); // Final randomness with deep mixing
+}
+
+uvec3 pcg3d(uvec3 v) {
+    v = v * uvec3(1664525u) + uvec3(1013904223u);
+    v.x += v.y * v.z;
+    v.y += v.z * v.x;
+    v.z += v.x * v.y;
+    v ^= v >> uvec3(16u);
+    v.x += v.y * v.z;
+    v.y += v.z * v.x;
+    v.z += v.x * v.y;
+    return v;
+}
+
+
 void main(){
-	gl_FragColor = vertColor;
+
+    vec2 st = gl_FragCoord.xy/iResolution.xy;
+
+    uvec3 rndVal = pcg3d(uvec3(st*iResolution.xy, iTime));
+    float rndValUnit = float(rndVal.x ^ rndVal.y ^ rndVal.z) / 4294967295.0;
+    vec3 color = vec3(rndValUnit);
+
+	gl_FragColor = vec4(color, 1.);
 }
