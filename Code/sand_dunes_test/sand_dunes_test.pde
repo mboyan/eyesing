882c0ffb4107476b422b3657c0c1a7bb5977ec58
Boyan Mihaylov <mih.boyan@gmail.com>
2026-08-31 19:03:27 +0200

    Deposition logic
    Needs debugging

--- Code/sand_dunes_test/sand_dunes_test.pde
+++ Code/sand_dunes_test/sand_dunes_test.pde
@@ -1,9 +1,12 @@
 PShader sandProjShader, sandDepositShader, noiseShader;
-PGraphics sandHeight, noiseGraphics, noiseModGraphics;
+PGraphics sandHeight, noiseGraphicsSel, noiseGraphicsDeposit, noiseModGraphics;
+
+float noiseTimeBias = 37318.3172;
 
 void setup(){
   size(500, 500, P2D);
   pixelDensity(1);
+  //frameRate(1);
   
   noiseModGraphics = createGraphics(width, height, P2D);
   noiseModGraphics.beginDraw();
@@ -12,6 +15,7 @@ void setup(){
   noiseModGraphics.endDraw();
   
   sandProjShader = loadShader("sand_proj_shader.glsl");
+  
   noiseShader = loadShader("noise_shader.glsl");
   noiseShader.set("iResolution", float(width), float(height), 0.0);
   noiseShader.set("iTime", 0.0);
@@ -29,25 +33,56 @@ void setup(){
   sandHeight.ellipse(0.5*width, 0.5*height, 100, 100);
   sandHeight.endDraw();
   
-  // Compute initial noise
-  noiseGraphics = createGraphics(width, height, P2D);
-  noiseGraphics.beginDraw();
-  noiseGraphics.shader(noiseShader);
-  noiseGraphics.fill(0);
-  noiseGraphics.rect(0, 0, width, height);
-  noiseGraphics.endDraw();
+  // Compute initial selection noise
+  noiseGraphicsSel = createGraphics(width, height, P2D);
+  noiseGraphicsSel.beginDraw();
+  noiseGraphicsSel.shader(noiseShader);
+  noiseGraphicsSel.fill(0);
+  noiseGraphicsSel.rect(0, 0, width, height);
+  noiseGraphicsSel.endDraw();
+  
+  // Compute initial deposition noise
+  noiseShader.set("iTime", noiseTimeBias);
+  noiseGraphicsDeposit = createGraphics(width, height, P2D);
+  noiseGraphicsDeposit.beginDraw();
+  noiseGraphicsDeposit.shader(noiseShader);
+  noiseGraphicsDeposit.fill(0);
+  noiseGraphicsDeposit.rect(0, 0, width, height);
+  noiseGraphicsDeposit.endDraw();
   
   sandProjShader.set("iResolution", float(width), float(height), 0.0);
   sandProjShader.set("heightTexture", sandHeight);
-  sandProjShader.set("noiseTexture", noiseGraphics);
+  sandProjShader.set("noiseTextureSel", noiseGraphicsSel);
+  sandProjShader.set("noiseTextureDeposit", noiseGraphicsDeposit);
   sandProjShader.set("windDir", 1.0, 1.0);
   sandProjShader.set("selDensity", exp(-0.05));
   sandProjShader.set("hopDist", 5.0);
 }
 
 void draw(){
+  println(frameCount);
+  // Update selection noise
+  noiseShader.set("iTime", float(frameCount));
+  noiseGraphicsSel.beginDraw();
+  noiseGraphicsSel.shader(noiseShader);
+  noiseGraphicsSel.fill(0);
+  noiseGraphicsSel.rect(0, 0, width, height);
+  noiseGraphicsSel.endDraw();
+  
+  // Update deposition noise
+  noiseShader.set("iTime", float(frameCount) + noiseTimeBias);
+  noiseGraphicsSel.beginDraw();
+  noiseGraphicsSel.shader(noiseShader);
+  noiseGraphicsSel.fill(0);
+  noiseGraphicsSel.rect(0, 0, width, height);
+  noiseGraphicsSel.endDraw();
+  
+  // Pass selection noise
+  sandProjShader.set("noiseTextureSel", noiseGraphicsSel);
+  sandProjShader.set("noiseTextureDeposit", noiseGraphicsDeposit);
+  
   shader(sandProjShader);
   rect(0, 0, width, height);
   //image(sandHeight, 0, 0);
-  //image(noiseGraphics, 0, 0);
+  //image(noiseGraphicsSel, 0, 0);
 }
--- Code/sand_dunes_test/sand_proj_shader.glsl
+++ Code/sand_dunes_test/sand_proj_shader.glsl
@@ -19,7 +19,8 @@ uniform vec4      iMouse;                // mouse pixel coords. xy: current (if 
 uniform vec4      iDate;                 // (year, month, day, time in seconds)
 
 uniform sampler2D heightTexture;
-uniform sampler2D noiseTexture;
+uniform sampler2D noiseTextureSel;
+uniform sampler2D noiseTextureDeposit;
 
 uniform vec2 windDir;
 uniform float selDensity;
@@ -27,8 +28,27 @@ uniform float hopDist;
 
 const int maxHops = 100;
 
+float checkShadow(vec2 currentPos, vec2 normWindDir, float sandHeight){
+    // Check if in shadow against wind direction
+    float maxShadowDist = length(iResolution.xy);
+    float shadowStepSize = 0.1;
+    int shadowSteps = int(floor(maxShadowDist / shadowStepSize));
+    float shadow = 0.0;
+    vec2 shadowProbePos = currentPos;
+    float shadowProbeHeight = 0.0;
+    for(int i = 0; i < shadowSteps; ++i)
+    {
+        shadowProbePos = round(shadowProbePos - shadowStepSize * normWindDir);
+        shadowProbeHeight = texture2D(heightTexture, shadowProbePos / iResolution.xy).x;
+        shadow = mix(step((i + 1) * shadowStepSize, sandHeight - shadowProbeHeight), shadow, shadow); // shadow turns true if height difference is larger than distance from source
+    }
+    return shadow;
+}
+
 void main(){
 	vec2 st = gl_FragCoord.xy/iResolution.xy;
+
+    // float diagLen = length(iResolution.xy);
 
     // Get sand height
     float sandHeight = texture2D(heightTexture, st).x;
@@ -37,37 +57,41 @@ void main(){
     vec2 normWindDir = normalize(windDir);
 
     // Select sand grains
-    float sel = step(selDensity, texture2D(noiseTexture, st).x);
+    float sel = step(selDensity, texture2D(noiseTextureSel, st).x);
     float sandCovered = 1.0 - step(0.0, -sandHeight);
     sel *= sandCovered;
 
-    // Check if in shadow against wind direction
-    float maxShadowDist = length(iResolution.xy);
-    float shadowStepSize = 0.1;
-    int shadowSteps = int(floor(maxShadowDist / shadowStepSize));
-    float shadow = 0.0;
-    vec2 shadowProbePos = gl_FragCoord.xy;
-    float shadowProbeHeight = 0.0;
-    for(int i = 0; i < shadowSteps; ++i)
-    {
-        shadowProbePos = round(shadowProbePos - shadowStepSize * normWindDir);
-        shadowProbeHeight = texture2D(heightTexture, shadowProbePos / iResolution.xy).x;
-        shadow = mix(step((i + 1) * shadowStepSize, sandHeight - shadowProbeHeight), shadow, shadow); // shadow turns true if height difference is larger than distance from source
-    }
+    float shadow = checkShadow(gl_FragCoord.xy, normWindDir, sandHeight);
 
     // Determine number of windward hops
     vec2 newPos = gl_FragCoord.xy;
     vec2 newPosCandidate;
     float newPosHeight;
+    float depositProb;
+    float candidateShadow;
+    float depositTry;
+    float deposited = 0.0;
+    float nHopsDeposited = 0.0;
     for(int i = 0; i < maxHops; ++i)
     {
         newPosCandidate = round(newPos + hopDist * windDir);
 
         // Probe new position
         newPosHeight = texture2D(heightTexture, newPosCandidate / iResolution.xy).x;
+        
+        // Check if candidate in shadow
+        candidateShadow = checkShadow(newPosCandidate, normWindDir, newPosHeight);
 
-        // newPos = mix(newPos, newPosHeight, );
+        // Deposition probability: 0.4 (bare) / 0.6 (covered) / 1.0 (in shadow)
+        depositProb = mix(mix(0.4, 0.6, 1.0 - step(0.0, -newPosHeight)), 1.0, candidateShadow);
+        depositTry = texture2D(noiseTextureDeposit, st).x;
+        deposited = mix(1.0 - step(depositProb, depositTry), deposited, 1.0 - step(0.0, -deposited));
+        newPos = mix(newPos, newPosCandidate, deposited);
+
+        // Save number of hops until deposition
+        nHopsDeposited = mix(nHopsDeposited + (i / maxHops), nHopsDeposited, deposited);
     }
 
-    gl_FragColor = vec4(vec3((1. - shadow) * sel), 1.0);
+    // gl_FragColor = vec4(vec3((1. - shadow) * sel), 1.0);
+    gl_FragColor = vec4(vec3(nHopsDeposited), 1.0);
 }
